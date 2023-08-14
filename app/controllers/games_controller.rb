40cfa4847726e9e314ce322dd08265260e2ed773
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-14 16:45:02 -0400

    progress on updating routes and setting methods to calculate the score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,10 @@
+require 'open-uri'
+require 'json'
 class GamesController < ApplicationController
 
     def new
       @letters = random_letters(10)
     end
-
-    private
 
     def random_letters(count)
       alphabet = ('A'..'Z').to_a
@@ -16,4 +16,48 @@ class GamesController < ApplicationController
 
       letters_array
     end
+
+    def score
+    # raise is to debug
+    @word = params[:word]
+    @letters = params[:letters]
+    end
+
+    # def calculate_time
+    #   start_time = session[:start_time]
+    #   end_time = Time.now
+    #   time_taken = end_time - start_time
+    #   time_taken
+    # end
+
+
+    # def calculate_score(word, time)
+    #   length_score = word.length
+    #   time_score = length_score / time
+    #   total_score = length_score * time_score
+    #   total_score
+    # end
+
+    def api_call(word)
+      url = "https://wagon-dictionary.herokuapp.com/#{word}"
+      response = open(url).read
+      JSON.parse(response)
+    end
+
+  #   def valid_word_check
+  #   if valid_word_check(@word, @letters)
+  #     api = api_call(@word)
+  #     if api['found']
+  #       @time = calculate_time
+  #       @score = calculate_score(@word, @time)
+  #       @message = "You won!"
+  #     else
+  #       @score = 0
+  #       @message = "doesn't seem to be a valid english word."
+  #     end
+  #   else
+  #     @score = 0
+  #     @message = "test can't be built out of those words."
+  #   end
+  # end
 end
